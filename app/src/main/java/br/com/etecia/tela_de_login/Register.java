7a02f11610470fd118dbfa7f3204237dd1214cb3
Caiopatucci <caiopatucci@hotmail.com>
2021-09-20 20:02:37 -0300

    first commit

---
+++ app/src/main/java/br/com/etecia/tela_de_login/Register.java
@@ -0,0 +1,14 @@
+package br.com.etecia.tela_de_login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Register extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register_layout);
+    }
+}
